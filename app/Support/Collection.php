1fb0b273cf6b6bf9d645ae5fc5b2e16ba548a5fe
Ian Lessa <ian.lessa1@gmail.com>
2017-05-28 20:40:00 -0300

    Métodos get e count de Collection implementados e testados

--- app/Support/Collection.php
+++ app/Support/Collection.php
@@ -11,11 +11,23 @@ namespace App\Support;
 
 class Collection
 {
+    private $items = [];
 
     /**
      * Collection constructor.
      */
-    public function __construct()
+    public function __construct(array $items = [])
     {
+        $this->items = $items;
+    }
+
+    public function get() : array
+    {
+        return $this->items;
+    }
+
+    public function count()
+    {
+        return count($this->items);
     }
 }
