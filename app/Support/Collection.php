7e7cbfb340f670772412c43164106b27215f3656
Ian Lessa <ian.lessa1@gmail.com>
2017-05-28 20:33:08 -0300

    Criado teste e Classe app/Support/Collection

---
+++ app/Support/Collection.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ian
+ * Date: 28/05/2017
+ * Time: 20:32
+ */
+
+namespace App\Support;
+
+
+class Collection
+{
+
+    /**
+     * Collection constructor.
+     */
+    public function __construct()
+    {
+    }
+}
